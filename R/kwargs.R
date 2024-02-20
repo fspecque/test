a0417e96e8c3ca1bbdef804bc79237acee8fa82b
Florian Specque <f.specque@hotmail.fr>
2024-02-20 16:31:49 +0100

    Implement a wrapper for scVI from scvi-tools

--- R/kwargs.R
+++ R/kwargs.R
@@ -44,5 +44,42 @@ args <- list(
     ),
     verbose = c("NOTSET"=0L, "DEBUG"=10L, "INFO"=20L, "WARNING"=30L,
                 "ERROR"=40L, "CRITICAL"=50L)
+  ),
+  scVI = list(
+    scVI = c(
+      # scvi.module.VAE
+      'n_input', 'n_batch', 'n_labels', 'n_continuous_cov', 'n_cats_per_cov',
+      'log_variational', 'encode_covariates', 'deeply_inject_covariates',
+      'use_batch_norm', 'use_layer_norm', 'use_size_factor_key',
+      'use_observed_lib_size', 'library_log_means', 'library_log_vars',
+      'var_activation', 'extra_encoder_kwargs',
+      'extra_decoder_kwargs'
+    ),
+    setup_anndata = c(
+      'size_factor_key', 'categorical_covariate_keys', 'continuous_covariate_keys'
+    ),
+    train = c(
+      'accelerator', 'devices', 'validation_size', 'shuffle_set_split',
+      'load_sparse_tensor', 'early_stopping', 'datasplitter_kwargs',
+      'plan_kwargs', 'data_module',
+      # scvi.train.Trainer
+      'benchmark', 'n_samples_per_label', 'check_val_every_n_epoch',
+      'default_root_dir', 'enable_checkpointing', 'checkpointing_monitor',
+      'num_sanity_val_steps', 'enable_model_summary', 'early_stopping',
+      'early_stopping_monitor', 'early_stopping_min_delta',
+      'early_stopping_patience', 'early_stopping_mode', 'additional_val_metrics',
+      'enable_progress_bar', 'progress_bar_refresh_rate', 'simple_progress_bar',
+      'logger', 'log_every_n_steps', 'learning_rate_monitor',
+      # pytorch.trainer.trainer.Trainer
+      'strategy', 'num_nodes', 'precision', 'callbacks', 'fast_dev_run',
+      'min_epochs', 'max_steps', 'min_steps', "max_time", 'limit_train_batches',
+      'limit_val_batches', 'limit_test_batches', 'limit_predict_batches',
+      'overfit_batches', 'val_check_interval', 'accumulate_grad_batches',
+      'gradient_clip_val', 'gradient_clip_algorithm', 'deterministic',
+      'inference_mode', 'use_distributed_sampler', 'profiler', 'detect_anomaly',
+      'barebones', 'plugins', 'sync_batchnorm', 'reload_dataloaders_every_n_epochs'
+    ),
+    verbose = c("NOTSET"=0L, "DEBUG"=10L, "INFO"=20L, "WARNING"=30L,
+                "ERROR"=40L, "CRITICAL"=50L)
   )
 )
---
+++ R/scVI.R
@@ -0,0 +1,213 @@
+#' @include kwargs.R
+#'
+NULL
+
+#' Run scVI on Seurat's \link[SeuratObject]{Assay5} object through \code{\link[Seurat]{IntegrateLayers}}
+#'
+#' @description
+#' A wrapper to run \code{scVI} on multi-layered Seurat V5 object.
+#' Requires a conda environment with \code{scvi-tools} and necessary dependencies
+#'
+#' Can be called via \code{SeuratIntegrate::scVIIntegration()} or
+#' \code{scVIIntegration.fix()}
+#'
+#' \strong{Recommendations}: use raw counts and all features
+#' (\code{features = Features(object), layers = "counts"})
+#'
+#' @inheritParams scANVIIntegration
+#' @inherit scANVIIntegration return
+#' @inheritParams integration-method
+#' @param groups A \bold{named} data frame with grouping information.
+#' @param conda_env Path to conda environment to run scVI (should also
+#' contain the scipy python module)
+#' @param model.save.dir Path to a directory to save the model to. Uses
+#' \code{SCVI.save()}. Does not save anndata. Note that neither the trainer
+#' optimizer state nor the trainer history are saved.
+#' \code{model.save.dir = NULL} (default) disables saving the model.
+#' @param ndims.out Number of dimensions for \code{new.reduction} output.
+#' Corresponds to \code{n_latent} argument in the original API of SCVI
+#' @param latent_distribution One of the following:
+#' \itemize{
+#'  \item{\code{normal}:} {Normal distribution (default)}
+#'  \item{\code{ln}:} {Logistic normal distribution (Normal(0, I) transformed by softmax)}
+#' }
+#' @param verbose.scvi Verbosity level of scVI. From quietest to talkiest:
+#' CRITICAL, ERROR, WARNING, INFO (default), DEBUG, NOTSET
+#' @param ... For \code{scVIIntegration()}, additional arguments to be passed to
+#' \code{scvi.model.SCVI}, \code{SCVI.setup_anndata} or \code{SCVI.train} (see
+#' \strong{Details} section). For \code{scVIIntegration.fix()}, all of the above
+#'
+#' @details
+#' This wrappers calls three python functions through \pkg{reticulate}.
+#' Find the \pkg{scVI}-specific arguments there:
+#' \itemize{
+#'   \item{model initiation:} {
+#'   \href{https://docs.scvi-tools.org/en/stable/api/reference/scvi.model.SCVI.html#scvi.model.SCVI}{scvi.model.SCVI}, which relies on
+#'   \href{https://docs.scvi-tools.org/en/stable/api/reference/scvi.module.VAE.html#scvi.module.VAE}{scvi.module.VAE}}
+#'   \item{anndata setup:} {
+#'   \href{https://docs.scvi-tools.org/en/stable/api/reference/scvi.model.SCVI.html#scvi.model.SCVI.setup_anndata}{SCVI.setup_anndata}}
+#'   \item{training:} {
+#'   \href{https://docs.scvi-tools.org/en/stable/api/reference/scvi.model.SCVI.html#scvi.model.SCVI.train}{SCVI.train}}
+#' }
+#'
+#' @importFrom reticulate use_condaenv import r_to_py py_to_r
+#' @importFrom Matrix t
+#' @importFrom Seurat CreateDimReducObject
+#' @importFrom SeuratObject JoinLayers GetAssayData
+#'
+#' @export
+#'@note This function requires the
+#' \href{https://scvi-tools.org/}{\pkg{scvi-tools}} package
+#' to be installed (along with \pkg{scipy})
+#'
+#' @examples
+#' \dontrun{
+#' # Preprocessing
+#' obj <- SeuratData::LoadData("pbmcsca")
+#' obj[["RNA"]] <- split(obj[["RNA"]], f = obj$Method)
+#' obj <- NormalizeData(obj)
+#' obj <- FindVariableFeatures(obj)
+#' obj <- ScaleData(obj)
+#' obj <- RunPCA(obj)
+#'
+#' # After preprocessing, we integrate layers:
+#' obj <- IntegrateLayers(object = obj, method = scVIIntegration,
+#'                        features = Features(obj), conda_env = 'scvi-tools',
+#'                        layers = 'counts', groups = obj[[]], groups.name = 'Method')
+#'
+#' # To enable cell label-guided correction, save the model, add other 'nuisance' factors and increase number of threads used:
+#' obj <- IntegrateLayers(object = obj, method = scVIIntegration,
+#'                        features = Features(obj), conda_env = 'scvi-tools',
+#'                        layers = 'counts', groups = obj[[]], groups.name = "Method",
+#'                        labels.name = "CellType",
+#'                        categorical_covariate_keys = list("Experiment"),
+#'                        continuous_covariate_keys = list("percent.mito"),
+#'                        ncores = 8, model.save.dir = '~/Documents/scVI.model')
+#' }
+#'
+#' @references Lopez, R., Regier, J., Cole, M. B., Jordan, M. I. & Yosef, N.
+#' Deep generative modeling for single-cell transcriptomics. Nat Methods 15,
+#' 1053–1058 (2018). \href{https://doi.org/10.1038/s41592-018-0229-2}{DOI}
+#'
+#' @seealso \code{\link[Seurat]{IntegrateLayers}}, \code{\link[Seurat]{writing-integration}}
+
+scVIIntegration <- function(
+    object,
+    groups = NULL,
+    groups.name = NULL,
+    labels.name = NULL,
+    features = NULL,
+    layers = 'counts',
+    scale.layer = 'scale.data',
+    conda_env = NULL,
+    new.reduction = 'integrated.scVI',
+    reduction.key = "scVIlatent_",
+    ncores = NULL,
+    model.save.dir = NULL,
+    # scvi.model.SCVI
+    ndims.out = 10,
+    n_hidden = 128L,
+    n_layers = 1L,
+    dropout_rate = 0.1,
+    dispersion = c('gene', 'gene-batch', 'gene-label', 'gene-cell'),
+    gene_likelihood = c("zinb", "nb", "poisson"),
+    latent_distribution = c('normal', 'ln'),
+    # SCVI.train
+    max_epochs = NULL,
+    train_size = 0.9,
+    batch_size = 128L,
+    seed.use = 42L,
+    verbose = TRUE,
+    verbose.scvi = c("INFO", "NOTSET", "DEBUG", "WARNING", "ERROR", "CRITICAL"),
+    ...){
+  dispersion <- match.arg(arg = dispersion)
+  gene_likelihood <- match.arg(arg = gene_likelihood)
+  verbose.scvi <- toupper(verbose.scvi)
+  verbose.scvi <- match.arg(arg = verbose.scvi)
+  conda_env %||% abort(message = "'conda_env' cannot be NULL")
+  varargs <- list(...)
+
+  use_condaenv(conda_env, required = TRUE)
+  sc <-  import('scanpy', convert = FALSE)
+  scvi <-  import('scvi', convert = FALSE)
+  seed.use %iff% { scvi$settings$seed = as.integer(x = seed.use) }
+  ncores %iff% { scvi$settings$num_threads = as.integer(x = ncores) }
+  scvi$settings$verbosity = args$scANVI$verbose[verbose.scvi]
+  scipy <-  import('scipy', convert = FALSE)
+
+  groups <- groups %||% Seurat:::CreateIntegrationGroups(object = object,
+                                                         layers = layers,
+                                                         scale.layer = scale.layer)
+  if (! inherits(x = groups, what = "data.frame")) {
+    # groups is supposedly a vector, a matrix or a list
+    groups <- as.data.frame(groups)
+  }
+  groups.name <- groups.name %||% colnames(groups)[1]
+  if(! length(intersect(colnames(groups), groups.name))) {
+    abort(message="'groups.name' not in 'groups' data frame")
+  }
+  if(length(intersect(colnames(groups), groups.name)) > 1) {
+    warning(paste("more 'groups.name' that expected. Using the first one",
+                  sQuote(x = groups.name[1])), call. = FALSE, immediate. = TRUE)
+    groups.name <- groups.name[1]
+  }
+  layer <- unique(sub("\\..*", "", layers %||% "counts"))
+  if(length(layer) > 1) {
+    abort(message="cannot find a consensus layer")
+  }
+
+  object <- JoinLayers(object = object, layers = layer)
+
+  adata <- sc$AnnData(
+    X   = scipy$sparse$csr_matrix(
+      t( GetAssayData(object, layer = layer)[features ,] )
+    ),
+    obs = r_to_py(groups),
+    var = r_to_py(features)
+  )
+
+  args.call <- c(list(adata = adata, labels_key = r_to_py(labels.name),
+                      layer = r_to_py(NULL),
+                      batch_key = r_to_py(groups.name)),
+                 varargs[intersect(names(varargs), args$scVI$setup_anndata)])
+  do.call(scvi$model$SCVI$setup_anndata, args.call)
+
+  args.call <- c(list(adata = adata, n_hidden = r_to_py(as.integer(n_hidden)),
+                      n_latent = r_to_py(as.integer(ndims.out)),
+                      n_layers = r_to_py(as.integer(n_layers)),
+                      dropout_rate = r_to_py(dropout_rate),
+                      dispersion = r_to_py(dispersion),
+                      gene_likelihood = r_to_py(gene_likelihood),
+                      latent_distribution = r_to_py(latent_distribution)),
+                 varargs[intersect(names(varargs), args$scVI$scVI)])
+  model = do.call(scvi$model$SCVI, args.call)
+
+  max_epochs <- max_epochs %iff% as.integer(x = max_epochs)
+  args.call <- c(list(max_epochs = r_to_py(max_epochs),
+                      train_size = r_to_py(train_size),
+                      batch_size = r_to_py(as.integer(batch_size))),
+                 varargs[intersect(names(varargs), args$scVI$train)])
+
+  do.call(model$train, args.call)
+
+  model.save.dir %iff% model$save(dir_path = path.expand(model.save.dir),
+                                  overwrite = TRUE,
+                                  save_anndata = F)
+
+  latent = model$get_latent_representation()
+  latent <- as.matrix(latent)
+  rownames(latent) <- py_to_r(adata$obs$index$values)
+  colnames(latent) <- paste0(new.reduction, "_", 1:ncol(latent))
+  suppressWarnings(latent.dr <- CreateDimReducObject(embeddings = latent, key = reduction.key))
+  output.list <- list(latent.dr)
+  names(output.list) <- new.reduction
+  return(output.list)
+}
+
+attr(x = scVIIntegration, which = 'Seurat.method') <- 'integration'
+
+#' @rdname scVIIntegration
+scVIIntegration.fix <- function(...) {
+  scVIIntegration(...)
+}
+attr(x = scVIIntegration.fix, which = 'Seurat.method') <- 'integration'
