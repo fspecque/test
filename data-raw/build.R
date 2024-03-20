a8e7891494ba1d8c495efbd01346d60183770b6b
fspecque <f.specque@hotmail.fr>
2024-03-20 12:05:05 +0100

    Add build commands

---
+++ data-raw/build.R
@@ -0,0 +1,10 @@
+# roxygen2::roxygenise()
+devtools::document()
+pkgload::dev_help("UpdateEnvCache")
+
+DESCRIPTION <- readLines("DESCRIPTION")
+pkg.version <- gsub("\\s*", "", sub("^Version:", "", DESCRIPTION[grepl("^Version", DESCRIPTION)]))
+archive.name <- paste0('SeuratIntegrate_', pkg.version,'.tar.gz')
+cmd = paste0("R CMD build ../Seurat-Integrate && mv ", archive.name,
+             " ../ && R CMD INSTALL ../", archive.name)
+system(cmd)
